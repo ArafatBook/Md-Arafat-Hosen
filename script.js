c7955877a6f0f27b8ea271103bf2cf01e8e64bd1
MdArafatHosentop <djarafathd@gmail.com>
2026-02-04 09:41:46 +0600

    Remove Bengali comments from copyToClipboard function
    Removed comments in Bengali from the copyToClipboard function.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Copy to Clipboard Function
 function copyToClipboard(elementId) {
-    // টেক্সট এলিমেন্ট খুঁজে বের করা
     var copyText = document.getElementById(elementId).innerText;
-
-    // ক্লিপবোর্ডে কপি করা
     navigator.clipboard.writeText(copyText).then(function() {
-        // সফল হলে এলার্ট দেখানো
         alert("Copied: " + copyText);
     }, function(err) {
-        // এরর হলে কনসোলে দেখানো
         console.error('Could not copy text: ', err);
     });
+}
+
+// Mobile Menu Toggle Function
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
 }
