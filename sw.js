607d475cad2118ac24079d7b2e8a776fdf687c9b
Md Arafat Hosen <djarafathd@gmail.com>
2026-03-03 02:28:42 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Arafat OS Service Worker Installed');
+});
 
+self.addEventListener('fetch', (e) => {
+  // এটি অ্যাপটিকে সচল রাখতে সাহায্য করে
+});
